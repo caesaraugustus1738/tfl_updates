f5e77801636dc2d5a658209b7f6a7d7da3a03930
caesaraugustus1738 <71141254+caesaraugustus1738@users.noreply.github.com>
2020-10-23 13:10:02 +0100

    Added a very basic logger. Logger + prints() = bloated code. I do not like..

--- main.py
+++ main.py
@@ -1,45 +1,82 @@
 import time
-import scrapetweet_v2 as st
+import scrapetweet as st
 import requests
 import traceback
 import tweepy
+import logging
+import os
+from pathlib import Path
+
+# Make log dir, if it doesn't exist
+cwd = Path(os.path.dirname(__file__))
+try:
+	os.mkdir(Path(cwd/'logs'))
+except FileExistsError:
+	pass
+
+# Create and configure logger
+now_time = (time.strftime('%Y_%m_%d_%H:%M:%S'))
+
+LOG_FORMAT = '%(levelname)s %(asctime)s - %(message)s'
+logging.basicConfig(filename=Path(cwd/'logs'/f'tfl_update_{now_time}.log'),
+					level=logging.DEBUG,
+					format=LOG_FORMAT)
+logger = logging.getLogger()
+logger.debug('Starting program')
+
+# -----------------------------------------------------------------
 
 def main():
-	tweet_format = st.FormatTweet()
+	tf = st.TweetFormatter()
 
 	while True:
-		print(tweet_format.previous_scrape)
 
 		try:
 			print('Attempting to scrape...')
-			scrape = st.scrape()
+			logger.debug('Scrape.')
+			
+			scrape = st.scrape_tfl_data()
+			
 			print('Data scraped!\n')
+		
 		except requests.exceptions.ConnectTimeout as e:
+			logger.debug('Connection timed out.')
 			print('Connection has timed out - ',e)
 			time.sleep(5)
 			continue
+		
 		except:
+			logger.debug('Other scrape error.')
 			print('An error I cannot trace has occurred. Waiting 5 secs until next scrape attempt.')
 			time.sleep(5)
 			continue
 
 		try:
 			print('Attempting to package tweets...')
-			package = tweet_format.package_tweets(scrape)
+			logger.debug('Pack tweets.')
+			
+			package = tf.format(scrape)
+			
 			print('Tweets packaged!\n')
 		except st.NoUpdateException as e:
+			logger.debug('Nothing to update.')
 			print('\n', e, '\n')
 			pass
 
 		try:
 			print('Attempting to Tweet...')
-			st.Tweeter().tweet(package)
+			logger.debug('Tweet.')
+			
+			st.TwitterAccess().tweet(package)
+			
 			print('Tweet successful!\n')
 		except tweepy.error.TweepError as e:
+			logger.debug('TweepError - probably a duplicate tweet.')
 			print(traceback.format_exc())
 			pass
 
 		print('Waiting 5 min until next scrape...')
+		logger.debug('Waiting 5 min until next scrape...')
 		time.sleep(300)
 
 if __name__ == '__main__':

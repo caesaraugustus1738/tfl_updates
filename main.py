95eb6c7e7d72d33b270396d8dc3d09873a97dd5a
caesaraugustus1738 <71141254+caesaraugustus1738@users.noreply.github.com>
2020-10-25 11:18:48 +0000

    Cleaned out print debug statements. Removed control for custom exception st.NoUpdateException which I do not think I need now.

--- main.py
+++ main.py
@@ -33,57 +33,40 @@ def main():
 	while True:
 
 		try:
-			print('Attempting to scrape...')
 			logger.debug('Scrape.')
-			
 			scrape = st.scrape_tfl_data()
-			
-			print('Data scraped!\n')
 		
 		except requests.exceptions.ConnectTimeout as e:
 			logger.debug('Connection timed out.')
-			print('Connection has timed out - ',e)
 			time.sleep(5)
 			continue
 		
 		except:
 			logger.debug('Other scrape error.')
-			print('An error I cannot trace has occurred. Waiting 5 secs until next scrape attempt.')
 			time.sleep(5)
 			continue
 
 		try:
-			print('Attempting to package tweets...')
 			logger.debug('Pack tweets.')
-			
 			package = tf.format(scrape)
 			
 			if not package:
-				print('All lines currently good service. Waiting 5 min to check again.')
+				logger.debug('No issues to report. Check again in 5 min.')
 				time.sleep(300)
 				continue
 
-			print('Tweets packaged!\n')
-		except st.NoUpdateException as e:
-			logger.debug('Nothing to update.')
-			print('\n', e, '\n')
-			pass
-
 		try:
-			print('Attempting to Tweet...')
 			logger.debug('Tweet.')
-			
 			st.TwitterAccess().tweet(package)
-			
-			print('Tweet successful!\n')
+
 		except tweepy.error.TweepError as e:
 			logger.debug('TweepError - probably a duplicate tweet.')
 			print(traceback.format_exc())
 			pass
 
-		print('Waiting 5 min until next scrape...')
 		logger.debug('Waiting 5 min until next scrape...')
 		time.sleep(300)
+
 
 if __name__ == '__main__':
 	main()
